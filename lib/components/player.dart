45defcf56dc32ca1e7b235866e11e4427f703720
SATHISH RAVICHANDRAN <ravichandran.sathish@gmail.com>
2019-11-27 19:57:25 -0500

    update player image

--- lib/components/player.dart
+++ lib/components/player.dart
@@ -1,5 +1,6 @@
 import 'package:first_game/game_controller.dart';
 import 'package:flame/flame.dart';
+import 'package:flame/position.dart';
 import 'package:flame/sprite.dart';
 import 'package:flutter/cupertino.dart';
 import 'dart:ui';
@@ -11,6 +12,7 @@ class Player {
   Rect playerRect;
   bool isDead = false;
   Sprite boy = Sprite('boy.png');
+  var position;
 
   Player(this.gameController) {
     maxHealth = currentHealth = 300;
@@ -21,12 +23,15 @@ class Player {
       size,
       size,
     );
+    position = Position(gameController.screenSize.width / 2 - size/2,
+      gameController.screenSize.height / 2 -size/2 );
   }
 
   void render(Canvas c){
+    final size = Position(gameController.tileSize * 1.5,gameController.tileSize * 1.5);
     //Paint color = Paint()..color = Color(0xFF0000FF);
     //c.drawRect(playerRect, color );
-    boy.render(c);
+    boy.renderPosition(c,position,size: size);
     
 
 
